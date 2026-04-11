5413c3b62b6d8beb2dc45e59633e284641c93b45
Kareem Agha <79729084+kareemagha@users.noreply.github.com>
2026-04-12 08:53:34 +1000

    bug: fixed bug inrenaming

--- segment.cpp
+++ segment.cpp
@@ -91,7 +91,7 @@ void display(ComponentType comp, int number) {
             text = _openMap;
             break;
     }
-    _multiplex(txtPattern, COMPONENT_HOLD_TIME);
+    _multiplex(txt, COMPONENT_HOLD_TIME);
 
     if(comp == OPEN_CIRCUIT) {
         _multiplex(_dashMap, 2000)
